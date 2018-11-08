4cea987ec878f8b6c6efab923dd9db1c4b790684
maild <maild@172.16.0.114>
2018-11-08 09:29:09 +0100

    init commit

---
+++ src/pruebacirculo/Circulito.java
@@ -0,0 +1,67 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacirculo;
+
+/**
+ *
+ * @author maild
+ */
+public class Circulito {
+    private int x;
+    private int y;
+    private double radio;
+    public Circulito()
+    {
+    }
+    // constructor
+    public Circulito( int valorX, int valorY, double valorRadio )
+    {
+    x = valorX;
+    y = valorY;
+    establecerRadio( valorRadio );
+    }
+    public void establecerX( int valorX )
+    {
+    x = valorX;
+    }
+    public int obtenerX()
+    {
+    return x;
+    }
+    public void establecerY( int valorY )
+    {
+    y = valorY;
+    }
+    public int obtenerY()
+    {
+    return y;
+    }
+    public void establecerRadio( double valorRadio )
+    {
+    radio = ( valorRadio < 0.0 ? 0.0 : valorRadio );
+    }
+    public double obtenerRadio()
+    {
+    return radio;
+    }
+    public double obtenerDiametro()
+    {
+    return 2 * radio;
+    }
+    public double obtenerCircunferencia()
+    {
+    return Math.PI * obtenerDiametro();
+    }
+    public double obtenerArea(){
+    return Math.PI * radio * radio;
+    }
+    public String toString()
+    {
+    return "Centro = [" + x + ", " + y + "]; Radio = " + radio;
+    }
+
+}
+
